28d7aa305633486746e10c25357be65f48fef7fb
Michael Chung <mchung@kanetix.ca>
2015-05-26 15:51:36 -0400

    Initial Commit

---
+++ RealmBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealmBug
+//
+//  Created by Michael Chung on 2015-05-26.
+//  Copyright (c) 2015 Michael Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealmBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealmBug
+//
+//  Created by Michael Chung on 2015-05-26.
+//  Copyright (c) 2015 Michael Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
